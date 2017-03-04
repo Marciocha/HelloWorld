386f0f2ed94509362e79c56bf04d6122c22a62c7
Marciocha <taleja555@interia.pl>
2017-03-04 12:43:42 +0100

    add final files

---
+++ src/com/sdajava/zad1/Main.java
@@ -0,0 +1,20 @@
+package com.sdajava.zad1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a=99;
+        int b=10;
+        int c=a+b;
+        int d=a*b;
+        int e=a/b;
+        int f=d+e;
+
+        System.out.println("Wynik" + c + f);
+        System.out.println(e*b);
+        System.out.println(f+a-b);
+        System.out.println("koniec");
+    }
+}
